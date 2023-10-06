fefe35417bff795727d8c58573b668021ae6bfc7
Zadkiel Aharonian <hello@zadkiel.fr>
2023-10-07 00:38:38 +0200

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
 
-: executing \'git --version\' with system git
+: executing 'git --version' with system git
 time /usr/bin/git --version
 
-: executing \'git branch\' with system git
+: executing 'git branch' with system git
 time /usr/bin/git branch
 
-: executing \'git --version\' with pkgx git
+: executing 'git --version' with pkgx git
 time pkgx git --version
 
-: executing \'git branch\' with pkgx git
+: executing 'git branch' with pkgx git
 time pkgx git branch
